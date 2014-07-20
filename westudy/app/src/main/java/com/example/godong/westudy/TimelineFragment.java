fdcf54c7ebe670064bab09d0e84342e7639d448e
Go Dong Hyun <godong9@gmail.com>
2014-07-20 15:02:43 +0900

    pull to refresh

--- westudy/app/src/main/java/com/example/godong/westudy/TimelineFragment.java
+++ westudy/app/src/main/java/com/example/godong/westudy/TimelineFragment.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.os.Handler;
 
 /**
  * Created by baggajin on 14. 7. 13..
  */
-public class TimelineFragment extends Fragment{
+public class TimelineFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener{
+
+    SwipeRefreshLayout swipeLayout;
 
     public TimelineFragment(){
 
@@ -22,9 +26,22 @@ public class TimelineFragment extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_timeline, container, false);
+        swipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_container);
+        swipeLayout.setOnRefreshListener(this);
+        swipeLayout.setColorScheme(android.R.color.holo_blue_bright, android.R.color.holo_green_light, android.R.color.holo_orange_light, android.R.color.holo_red_light);
 
-        /** Inflate the layout for this fragment **/
-        return inflater.inflate(R.layout.fragment_timeline, container, false);
+        return view;
+    }
+
+    @Override
+    public void onRefresh() {
+        // TODO Auto-generated method stub
+        new Handler().postDelayed(new Runnable() {
+            @Override public void run() {
+                swipeLayout.setRefreshing(false);
+            }
+        }, 2000);
     }
 
 }
