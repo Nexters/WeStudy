dd4a386043885359d0b17a37885a02f3cac048d3
Go Dong Hyun <godong9@gmail.com>
2014-07-22 00:32:47 +0900

    일단 되는 상태 ㅡ

--- westudy/app/src/main/java/com/example/godong/westudy/CalendarFragment.java
+++ westudy/app/src/main/java/com/example/godong/westudy/CalendarFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.Toast;
 
 /**
  * Created by baggajin on 14. 7. 13..
@@ -22,9 +25,19 @@ public class CalendarFragment extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        CalendarView cal = (CalendarView) view.findViewById(R.id.calendarView);
+        cal.setOnDateChangeListener(new OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+                // TODO Auto-generated method stub
 
-        /** Inflate the layout for this fragment **/
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+                Toast.makeText(getActivity(),"Selected Date is\n\n"
+                                +dayOfMonth+" : "+month+" : "+year ,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
-
 }
