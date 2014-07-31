99932aab7669ea02698d8f9ba80a633223546584
Go Dong Hyun <godong9@gmail.com>
2014-07-31 23:47:25 +0900

    test 완료

--- my_server/controllers/testCtrl.js
+++ my_server/controllers/testCtrl.js
@@ -40,7 +40,7 @@ TestCtrl.getUser = function (req, res) {
 TestCtrl.addStudy = function(req, res) {
   var studies = [
     {
-      creator:'test',
+      creator: 'test',
       subject: 'language',
       title: '신촌 영어회화 스터디',
       number_type: 5,
@@ -48,21 +48,21 @@ TestCtrl.addStudy = function(req, res) {
       day_of_week: ['mon'],
       detail: '함께 해요',
       members: ['test'],
-      create_time : new Date()
+      create_time: new Date()
     },
     {
-      creator:'test2',
+      creator: 'test2',
       subject: 'language',
       title: '잠실 토익 스터디',
       number_type: 3,
       location: ['gangdong'],
-      day_of_week: ['mon','tue'],
+      day_of_week: ['mon', 'tue'],
       detail: '함께 토익해요',
       members: ['test2'],
-      create_time : new Date()
+      create_time: new Date()
     },
     {
-      creator:'test3',
+      creator: 'test3',
       subject: 'it',
       title: '신촌 프로그램 스터디',
       number_type: 7,
@@ -70,26 +70,27 @@ TestCtrl.addStudy = function(req, res) {
       day_of_week: ['sat'],
       detail: '함께 프로그램해요',
       members: ['test3'],
-      create_time : new Date()
+      create_time: new Date()
     },
     {
-      creator:'test4',
+      creator: 'test4',
       subject: 'language',
       title: '신촌 영어 회화같이해요',
       number_type: 5,
       location: ['sinchon'],
-      day_of_week: ['mon','wed'],
+      day_of_week: ['mon', 'wed'],
       detail: '함께 해요',
       members: ['test4'],
-      create_time : new Date()
+      create_time: new Date()
     }
   ];
-  Study.collection.remove({}, function(err) {
-    Study.collection.insert(studies,function(err, result) {
-      if(err) return res.send(400,err);
-      res.send(200,result);
+  Study.collection.remove({}, function (err) {
+    Study.collection.insert(studies, function (err, result) {
+      if (err) return res.send(400, err);
+      res.send(200, result);
     });
   });
+};
 
 TestCtrl.addArticle = function (req, res) {
   var testArticle = [
@@ -98,7 +99,7 @@ TestCtrl.addArticle = function (req, res) {
       'study_id': "study01",
       'contents': {
         'text': "넥스터즈 모임 끝나고 네오위즈에서 한컷~",
-        'photo_url': "nexters_140721",
+        'photo_url': "nexters_140721"
       },
       'create_time': new Date(14, 06, 21)
     },
@@ -107,7 +108,7 @@ TestCtrl.addArticle = function (req, res) {
       'study_id': "study01",
       'contents': {
         'text': "오늘 개발과정 안드로이드 메이븐, 그래들로 개발하기",
-        'photo_url': "",
+        'photo_url': ""
       },
       'create_time': new Date(14, 05, 21)
     },
@@ -116,7 +117,7 @@ TestCtrl.addArticle = function (req, res) {
       'study_id': "study01",
       'contents': {
         'text': "엠티갔다왔어요~",
-        'photo_url': "nexters_140605",
+        'photo_url': "nexters_140605"
       },
       'create_time': new Date(14, 05, 05)
     },
@@ -125,7 +126,7 @@ TestCtrl.addArticle = function (req, res) {
       'study_id': "study01",
       'contents': {
         'text': "오늘 5기 모집 마감합니다~ 많은 분들의 지원 감사합니다 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ캬캬캬캬카캬카캬카",
-        'photo_url': "",
+        'photo_url': ""
       },
       'create_time': new Date(14, 04, 06)
     },{
@@ -133,7 +134,7 @@ TestCtrl.addArticle = function (req, res) {
       'study_id': "study01",
       'contents': {
         'text': "오늘 개발회의 진행사항 서버 node js 세팅하기 세팅하는데만 3시간...",
-        'photo_url': "nexters_140503",
+        'photo_url': "nexters_140503"
       },
       'create_time': new Date(14, 04, 03)
     },
@@ -142,7 +143,7 @@ TestCtrl.addArticle = function (req, res) {
       'study_id': "study01",
       'contents': {
         'text': "오늘 과제들 다들 안해오셨네요 다음엔 더 꼼꼼히",
-        'photo_url': "",
+        'photo_url': ""
       },
       'create_time': new Date(14, 03, 24)
     },
@@ -151,7 +152,7 @@ TestCtrl.addArticle = function (req, res) {
       'study_id': "study01",
       'contents': {
         'text': "프로젝트 기획 : 스터디의 모든 것 완료!",
-        'photo_url': "nexters_140301",
+        'photo_url': "nexters_140301"
       },
       'create_time': new Date(14, 02, 01)
     },
@@ -160,7 +161,7 @@ TestCtrl.addArticle = function (req, res) {
       'study_id': "study01",
       'contents': {
         'text': "테스트 텍스트 아하하하하 히히히 히이히이히이이이이잉 말 소리",
-        'photo_url': "",
+        'photo_url': ""
       },
       'create_time': new Date(14, 01, 22)
     },{
@@ -168,7 +169,7 @@ TestCtrl.addArticle = function (req, res) {
       'study_id': "study01",
       'contents': {
         'text': "테스트 텍스트 이제 할 말이 없다 ",
-        'photo_url': "",
+        'photo_url': ""
       },
       'create_time': new Date(14, 01, 16)
     },
@@ -177,7 +178,7 @@ TestCtrl.addArticle = function (req, res) {
       'study_id': "study01",
       'contents': {
         'text': "사진 첨부한 텍스트 테스트 !!! 텍스트 테테테테스스ㅡ트트테",
-        'photo_url': "nexters_140125",
+        'photo_url': "nexters_140125"
       },
       'create_time': new Date(14, 00, 25)
     },
@@ -186,7 +187,7 @@ TestCtrl.addArticle = function (req, res) {
       'study_id': "study01",
       'contents': {
         'text': "test04 study01 아이디 룰루 랄라 룰루 ㄹ루루룰루 박가진 오늘 불참",
-        'photo_url': "",
+        'photo_url': ""
       },
       'create_time': new Date(14, 00, 15)
     },
@@ -195,7 +196,7 @@ TestCtrl.addArticle = function (req, res) {
       'study_id': "study01",
       'contents': {
         'text': "서버 개발 부진 안드로이드 개발 짱짱",
-        'photo_url': "",
+        'photo_url': ""
       },
       'create_time': new Date(14, 00, 05)
     }
