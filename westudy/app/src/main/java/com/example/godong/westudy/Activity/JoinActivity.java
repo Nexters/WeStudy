48dca1ff53231b60f558a0f23b325a77b10b0f4f
Go Dong Hyun <godong9@gmail.com>
2014-08-14 22:08:29 +0900

    타이틀바 적용

--- westudy/app/src/main/java/com/example/godong/westudy/Activity/JoinActivity.java
+++ westudy/app/src/main/java/com/example/godong/westudy/Activity/JoinActivity.java
@@ -1,5 +1,6 @@
 package com.example.godong.westudy.Activity;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -45,6 +46,22 @@ public class JoinActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        actionBar.setCustomView(R.layout.custom_title);
+
+//        ActionBar actionBar = getActionBar();
+//        actionBar.setDisplayShowTitleEnabled(true);
+//        actionBar.setIcon(R.drawable.common_logo);
+//
+//        actionBar.setCustomView(getLayoutInflater().inflate(R.layout.activity_join, null),
+//                new ActionBar.LayoutParams(
+//                        ActionBar.LayoutParams.WRAP_CONTENT,
+//                        ActionBar.LayoutParams.MATCH_PARENT,
+//                        Gravity.CENTER
+//                )
+//        );
 
         init();
     }
--- westudy/app/src/main/java/com/example/godong/westudy/Activity/StudyMainActivity.java
+++ westudy/app/src/main/java/com/example/godong/westudy/Activity/StudyMainActivity.java
@@ -46,13 +46,13 @@ public class StudyMainActivity extends FragmentActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_main);
-//        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.custom_title);
 
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        actionBar.setCustomView(R.layout.custom_title);
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-//        mTitle = getTitle();
 
         /** drawer Setup **/
         mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout)findViewById(R.id.drawer_layout));
