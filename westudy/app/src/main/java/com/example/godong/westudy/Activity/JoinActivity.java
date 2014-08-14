14277150e7a555882e29c1b27400379948fdd783
Go Dong Hyun <godong9@gmail.com>
2014-08-14 21:32:42 +0900

    커스텀 타이틀 삭제

--- westudy/app/src/main/java/com/example/godong/westudy/Activity/JoinActivity.java
+++ westudy/app/src/main/java/com/example/godong/westudy/Activity/JoinActivity.java
@@ -44,10 +44,7 @@ public class JoinActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_join);
-        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.custom_title);
 
         init();
     }
