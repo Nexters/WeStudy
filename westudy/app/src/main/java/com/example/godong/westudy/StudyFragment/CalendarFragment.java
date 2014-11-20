a296586237403bd9db1c4916eaf291d3844b2910
Go Dong Hyun <godong9@gmail.com>
2014-11-20 22:23:04 +0900

    로고 변경 및 디자인 변경

--- westudy/app/src/main/java/com/example/godong/westudy/StudyFragment/CalendarFragment.java
+++ westudy/app/src/main/java/com/example/godong/westudy/StudyFragment/CalendarFragment.java
@@ -1,5 +1,6 @@
 package com.example.godong.westudy.StudyFragment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.provider.CalendarContract;
 import android.support.v4.app.Fragment;
@@ -11,14 +12,53 @@ import android.widget.CalendarView;
 import android.widget.CalendarView.OnDateChangeListener;
 import android.widget.Toast;
 
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
 import com.example.godong.westudy.R;
 
 import java.util.GregorianCalendar;
 
 
-public class CalendarFragment extends Fragment {
-    private CalendarView cal;
 
+/**
+ * @author Mukesh Y
+ */
+public class CalendarFragment extends Fragment {
+    private Context context;
+    private View view;
+    public GregorianCalendar month, itemmonth;// calendar instances.
+
+    public CalendarAdapter adapter;// adapter instance
+    public Handler handler;// for grabbing some event values for showing the dot
+    // marker.
+    public ArrayList<String> items; // container to store calendar items which
+    // needs showing the event marker
+    ArrayList<String> event;
+    LinearLayout rLayout;
+    ArrayList<String> date;
+    ArrayList<String> desc;
 
     public static CalendarFragment newInstance(){
 
@@ -27,56 +67,242 @@ public class CalendarFragment extends Fragment {
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
+        context = getActivity();
         super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
-        init(view);
+        view = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        Locale.setDefault(Locale.US);
+
+        rLayout = (LinearLayout) view.findViewById(R.id.text);
+        month = (GregorianCalendar) GregorianCalendar.getInstance();
+        itemmonth = (GregorianCalendar) month.clone();
+
+        items = new ArrayList<String>();
+
+        adapter = new CalendarAdapter(context, month);
+
+        GridView gridview = (GridView) view.findViewById(R.id.gridview);
+        gridview.setAdapter(adapter);
+
+        handler = new Handler();
+        handler.post(calendarUpdater);
+
+        TextView title = (TextView) view.findViewById(R.id.title);
+        title.setText(android.text.format.DateFormat.format("MMMM yyyy", month));
+
+        RelativeLayout previous = (RelativeLayout) view.findViewById(R.id.previous);
+
+        previous.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setPreviousMonth();
+                refreshCalendar();
+            }
+        });
+
+        RelativeLayout next = (RelativeLayout) view.findViewById(R.id.next);
+        next.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setNextMonth();
+                refreshCalendar();
+
+            }
+        });
+
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                // removing the previous view if added
+                if (((LinearLayout) rLayout).getChildCount() > 0) {
+                    ((LinearLayout) rLayout).removeAllViews();
+                }
+                desc = new ArrayList<String>();
+                date = new ArrayList<String>();
+                ((CalendarAdapter) parent.getAdapter()).setSelected(v);
+                String selectedGridDate = CalendarAdapter.dayString
+                        .get(position);
+                String[] separatedTime = selectedGridDate.split("-");
+                String gridvalueString = separatedTime[2].replaceFirst("^0*",
+                        "");// taking last part of date. ie; 2 from 2012-12-02.
+                int gridvalue = Integer.parseInt(gridvalueString);
+                // navigate to next or previous month on clicking offdays.
+                if ((gridvalue > 10) && (position < 8)) {
+                    setPreviousMonth();
+                    refreshCalendar();
+                } else if ((gridvalue < 7) && (position > 28)) {
+                    setNextMonth();
+                    refreshCalendar();
+                }
+                ((CalendarAdapter) parent.getAdapter()).setSelected(v);
+
+                for (int i = 0; i < Utility.startDates.size(); i++) {
+                    if (Utility.startDates.get(i).equals(selectedGridDate)) {
+                        desc.add(Utility.nameOfEvent.get(i));
+                    }
+                }
+
+                if (desc.size() > 0) {
+                    for (int i = 0; i < desc.size(); i++) {
+                        TextView rowTextView = new TextView(context);
+
+                        // set some properties of rowTextView or something
+                        rowTextView.setText("Event:" + desc.get(i));
+                        rowTextView.setTextColor(Color.BLACK);
+
+                        // add the textview to the linearlayout
+                        rLayout.addView(rowTextView);
+
+                    }
+
+                }
+
+                desc = null;
+
+            }
+
+        });
+
+
 
         return view;
     }
 
-    private void init(View view) {
-        cal = (CalendarView) view.findViewById(R.id.calendarView);
-        cal.setOnDateChangeListener(new OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month,
-                                            int dayOfMonth) {
 
-                Toast.makeText(getActivity(),"Selected Date is\n\n"
-                                +dayOfMonth+" : "+month+" : "+year ,
-                        Toast.LENGTH_SHORT).show();
+    protected void setNextMonth() {
+        if (month.get(GregorianCalendar.MONTH) == month
+                .getActualMaximum(GregorianCalendar.MONTH)) {
+            month.set((month.get(GregorianCalendar.YEAR) + 1),
+                    month.getActualMinimum(GregorianCalendar.MONTH), 1);
+        } else {
+            month.set(GregorianCalendar.MONTH,
+                    month.get(GregorianCalendar.MONTH) + 1);
+        }
 
-            }
-        });
     }
 
-//    private void setCalendarEvent() {
-//        Intent intent = new Intent(Intent.ACTION_INSERT);
-//        intent.setType("vnd.android.cursor.item/event");
-//        intent.putExtra(CalendarContract.Events.TITLE, "Learn Android");
-//        intent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Home suit home");
-//        intent.putExtra(CalendarContract.Events.DESCRIPTION, "Download Examples");
-//
-//// Setting dates
-//        GregorianCalendar calDate = new GregorianCalendar(2014, 8, 20);
-//        intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
-//                calDate.getTimeInMillis());
-//        intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
-//                calDate.getTimeInMillis());
-//
-//// make it a full day event
-//        intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
-//
-//// make it a recurring Event
-//        intent.putExtra(CalendarContract.Events.RRULE, "FREQ=WEEKLY;COUNT=11;WKST=SU;BYDAY=TU,TH");
-//
-//// Making it private and shown as busy
-//        intent.putExtra(CalendarContract.Events.ACCESS_LEVEL, CalendarContract.Events.ACCESS_PRIVATE);
-//        intent.putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY);
-//    }
+    protected void setPreviousMonth() {
+        if (month.get(GregorianCalendar.MONTH) == month
+                .getActualMinimum(GregorianCalendar.MONTH)) {
+            month.set((month.get(GregorianCalendar.YEAR) - 1),
+                    month.getActualMaximum(GregorianCalendar.MONTH), 1);
+        } else {
+            month.set(GregorianCalendar.MONTH,
+                    month.get(GregorianCalendar.MONTH) - 1);
+        }
+
+    }
+
+    protected void showToast(String string) {
+        Toast.makeText(context, string, Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void refreshCalendar() {
+        TextView title = (TextView) view.findViewById(R.id.title);
+
+        adapter.refreshDays();
+        adapter.notifyDataSetChanged();
+        handler.post(calendarUpdater); // generate some calendar items
+
+        title.setText(android.text.format.DateFormat.format("MMMM yyyy", month));
+    }
+
+    public Runnable calendarUpdater = new Runnable() {
+
+        @Override
+        public void run() {
+            items.clear();
+
+            // Print dates of the current week
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+            String itemvalue;
+//            event = Utility.readCalendarEvent(context);
+//            Log.d("=====Event====", event.toString());
+            Log.d("=====Date ARRAY====", Utility.startDates.toString());
+
+            for (int i = 0; i < Utility.startDates.size(); i++) {
+                itemvalue = df.format(itemmonth.getTime());
+                itemmonth.add(GregorianCalendar.DATE, 1);
+                items.add(Utility.startDates.get(i).toString());
+            }
+            adapter.setItems(items);
+            adapter.notifyDataSetChanged();
+        }
+    };
 }
+
+
+//
+//
+//public class CalendarFragment extends Fragment {
+//    private CalendarView cal;
+//
+//
+//    public static CalendarFragment newInstance(){
+//
+//        CalendarFragment fragment = new CalendarFragment();
+//
+//        return fragment;
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState){
+//        super.onCreate(savedInstanceState);
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+//        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+//        init(view);
+//
+//        return view;
+//    }
+//
+//    private void init(View view) {
+////        cal = (CalendarView) view.findViewById(R.id.calendarView);
+////        cal.setOnDateChangeListener(new OnDateChangeListener() {
+////            @Override
+////            public void onSelectedDayChange(CalendarView view, int year, int month,
+////                                            int dayOfMonth) {
+////
+////                Toast.makeText(getActivity(),"Selected Date is\n\n"
+////                                +dayOfMonth+" : "+month+" : "+year ,
+////                        Toast.LENGTH_SHORT).show();
+////
+////            }
+////        });
+//    }
+//
+////    private void setCalendarEvent() {
+////        Intent intent = new Intent(Intent.ACTION_INSERT);
+////        intent.setType("vnd.android.cursor.item/event");
+////        intent.putExtra(CalendarContract.Events.TITLE, "Learn Android");
+////        intent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Home suit home");
+////        intent.putExtra(CalendarContract.Events.DESCRIPTION, "Download Examples");
+////
+////// Setting dates
+////        GregorianCalendar calDate = new GregorianCalendar(2014, 8, 20);
+////        intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+////                calDate.getTimeInMillis());
+////        intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+////                calDate.getTimeInMillis());
+////
+////// make it a full day event
+////        intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
+////
+////// make it a recurring Event
+////        intent.putExtra(CalendarContract.Events.RRULE, "FREQ=WEEKLY;COUNT=11;WKST=SU;BYDAY=TU,TH");
+////
+////// Making it private and shown as busy
+////        intent.putExtra(CalendarContract.Events.ACCESS_LEVEL, CalendarContract.Events.ACCESS_PRIVATE);
+////        intent.putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY);
+////    }
+//}
